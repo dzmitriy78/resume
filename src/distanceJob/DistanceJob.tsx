b05282b797bd1eed13858c8e7dbe8afd9e83eb9e
dzmitriy78 <93944218+dzmitriy78@users.noreply.github.com>
2022-06-26 22:44:23 +0300

    few fixed

--- src/distanceJob/DistanceJob.tsx
+++ src/distanceJob/DistanceJob.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import styles from "./DistanceJob.module.scss"
 import stylesContainer from "../common/styles/Container.module.scss"
 import ReactTypingEffect from 'react-typing-effect';
+
 const Fade = require("react-reveal/Fade")
 
 const DistanceJob = () => {
     return (
         <div className={styles.block}>
             <Fade right big>
-            <div className={`${stylesContainer.container} ${styles.DJContainer}`}>
-                <h2 className={styles.title}><ReactTypingEffect  text={"Рассматриваю варианты удаленной работы"}/></h2>
-                <button className={styles.btn}>Нанять меня</button>
-            </div>
-                </Fade>
+                <div className={`${stylesContainer.container} ${styles.DJContainer}`}>
+                    <h2 className={styles.title}>
+                        <ReactTypingEffect text={"Рассматриваю варианты удаленной работы"}
+                                           speed={100}
+                                           eraseSpeed={50}/>
+                    </h2>
+                    <button className={styles.btn}>Нанять меня</button>
+                </div>
+            </Fade>
         </div>
     );
 };
--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -13,8 +13,8 @@ const Main = () => {
             <div className={stylesContainer.container}>
                 <div className={style.mainText}>
                     <h4>Hello,</h4>
-                    <h1 className={style.root}>I'm Dzmitry Karas</h1>
-                    <h4><ReactTypingEffect text={"Frontend Developer"}/></h4>
+                    <h1 className={style.title}>I'm Dzmitry Karas</h1>
+                    <h4><ReactTypingEffect text={"Frontend developer"} speed={100}/></h4>
                 </div>
                 <div className={style.mainPhoto}>
                 </div>
