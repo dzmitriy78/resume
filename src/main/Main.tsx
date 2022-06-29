c9a2fe07e1281e1885b8636a0ade81e271794c1a
dzmitriy78 <93944218+dzmitriy78@users.noreply.github.com>
2022-06-29 22:16:22 +0300

    add visualisation

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -3,21 +3,28 @@ import style from "./Main.module.scss";
 import stylesContainer from "../common/styles/Container.module.scss";
 import TsParticles from "../common/components/TsParticles";
 import ReactTypingEffect from 'react-typing-effect';
+import rootPhoto from "./../assets/images/rootPhoto.jpg"
+import ReactParallaxTilt from "react-parallax-tilt";
 
 
 const Main = () => {
 
     return (
         <div className={style.mainBlock}>
-            <TsParticles/>
+           <div className={style.particles}>
+               <TsParticles/>
+           </div>
             <div className={stylesContainer.container}>
                 <div className={style.mainText}>
-                    <h4>Hello,</h4>
-                    <h1 className={style.title}>I'm Dzmitry Karas</h1>
-                    <h4><ReactTypingEffect text={"Frontend developer"} speed={100}/></h4>
+                    <h1>Hello,</h1>
+                    <p className={style.title}>I am Dzmitry Karas</p>
+                    <h1><ReactTypingEffect text={"Front-end developer"} speed={100}/></h1>
                 </div>
+                <ReactParallaxTilt>
                 <div className={style.mainPhoto}>
+                    <img className={style.rootPhoto} src={rootPhoto} alt={"rootPhoto"}/>
                 </div>
+                </ReactParallaxTilt>
             </div>
         </div>
     );
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import styles from "./skill.module.scss"
+import ReactParallaxTilt from "react-parallax-tilt";
 
 interface SkillPropsType {
     title: string
     description: string
-    style:  any
+    style: any
 }
 
 const Skill = (props: SkillPropsType) => {
     return (
-        <div className={styles.skill} >
-            <div className={styles.icon} style={props.style}> </div>
-            <h3>{props.title}</h3>
-            <span className={styles.skillDescription}>
+        <ReactParallaxTilt>
+            <div className={styles.skill}>
+                <div className={styles.icon} style={props.style}></div>
+                <h3>{props.title}</h3>
+                <span className={styles.skillDescription}>
                 {props.description}
             </span>
-        </div>
+            </div>
+        </ReactParallaxTilt>
     );
 };
 
